f9a745029df299db7f66c38832b78850c5c64f9f
sohann777 <sohann7704@gmail.com>
2024-06-22 16:42:38 +0530

    Initial commit

---
+++ src/Components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center text-white">
+        S O H A N
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
+        <FaLinkedin></FaLinkedin>
+        <FaGithub />
+        <FaSquareXTwitter />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
