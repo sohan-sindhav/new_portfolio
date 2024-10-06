ea7aee0df81f2bad9acd63ea4b4a161408cd2f8f
sohan sindhav <116996300+sohan-sindhav@users.noreply.github.com>
2024-10-06 12:47:22 +0530

    Update Contact.jsx( ph.no removed)

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,14 +15,7 @@ function Contact() {
       </motion.h2>
       <div className="text-center tracking-tighter ">
         {/* <p className="my-4">{CONTACT.address}</p> */}
-        <motion.p
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1 }}
-          className="my-4"
-        >
-          {CONTACT.phoneNo}
-        </motion.p>
+        
         <motion.a
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
