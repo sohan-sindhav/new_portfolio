95375cf1b2bf4ad08e2400f87ab7c62490244449
sohan sindhav <116996300+sohan-sindhav@users.noreply.github.com>
2025-01-27 22:52:33 +0530

    rename : full Stack dev to software dev

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -32,7 +32,7 @@ function Hero() {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent text-center lg:text-left "
             >
-              Full Stack Developer
+              Software Developer
             </motion.span>
             <motion.p
               variants={container(1)}
